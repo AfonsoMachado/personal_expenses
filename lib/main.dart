eb85ee4afa06a81aabea58d4bc4a79e5bf2fea75
Afonso Machado <afonsosmachado@gmail.com>
2023-09-25 11:47:37 -0300

    feat: create transactions model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/models/transaction.dart';
 
 main() => runApp(const ExpansesApp());
 
@@ -7,19 +8,52 @@ class ExpansesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Tênis',
+      value: 3190.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Tênis 2',
+      value: 390.76,
+      date: DateTime.now(),
+    ),
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("init"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: const Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Text('Lista'),
+          )
+        ],
+      ),
     );
   }
 }
