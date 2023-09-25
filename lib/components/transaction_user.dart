845e75f996c08ebdd6b7d17ca6438828ce89f161
Afonso Machado <afonsosmachado@gmail.com>
2023-09-25 16:11:36 -0300

    refactor: pass data to parent component

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/components/transaction_form.dart';
 import 'package:personal_expenses/components/transaction_list.dart';
@@ -26,12 +28,26 @@ class _TransactionUserState extends State<TransactionUser> {
     ),
   ];
 
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    // Quando um estado mudo, a árvore de componentes tambem muda automaticamente
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TransactionList(_transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
     );
   }
